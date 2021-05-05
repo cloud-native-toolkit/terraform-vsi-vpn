b37181dba67818d63f226eebbe2caa5032b6b302
Sean Sundberg <seansund@us.ibm.com>
2021-05-05 12:14:16 -0500

    Bumps bastion module to v1.3.1
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data ibm_is_vpc vpc {
 }
 
 module "openvpn-server" {
-  source = "github.com/cloud-native-toolkit/terraform-vsi-bastion.git?ref=v1.3.0"
+  source = "github.com/cloud-native-toolkit/terraform-vsi-bastion.git?ref=v1.3.1"
 
   resource_group_id    = var.resource_group_id
   region               = var.region
