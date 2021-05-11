f4dd12869894938c7fd401badb40de56ff16b324
Sean Sundberg <seansund@us.ibm.com>
2021-05-10 21:54:33 -0500

    Sets ACLs to everything for openvpn ACLs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-openvpn-subnets.tf
+++ test/stages/stage2-openvpn-subnets.tf
@@ -10,28 +10,16 @@ module "openvpn-subnets" {
   _count            = 1
   label             = "openvpn"
   acl_rules          = [{
-    name="ingress-ssh"
+    name="ingress-all"
     action="allow"
     direction="inbound"
     source="0.0.0.0/0"
     destination="0.0.0.0/0"
-    tcp = {
-      port_min=22
-      port_max=22
-      source_port_min=22
-      source_port_max=22
-    }
   }, {
-    name="egress-ssh"
+    name="egress-all"
     action="allow"
     direction="outbound"
     destination="0.0.0.0/0"
     source="0.0.0.0/0"
-    tcp = {
-      port_min=22
-      port_max=22
-      source_port_min=22
-      source_port_max=22
-    }
   }]
 }
