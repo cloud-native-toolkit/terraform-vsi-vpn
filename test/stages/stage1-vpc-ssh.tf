7b48a71916fb6aaf49c8ec2c7f1516f511780d1a
Sean Sundberg <seansund@us.ibm.com>
2021-05-20 18:39:41 -0500

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-vpc-ssh.tf
+++ test/stages/stage1-vpc-ssh.tf
@@ -3,7 +3,7 @@ module "vpcssh" {
 
   resource_group_name = module.resource_group.name
   region              = var.region
-  name_prefix         = "${var.name_prefix}2"
+  name_prefix         = "vpc-test-ssh"
   ibmcloud_api_key    = var.ibmcloud_api_key
   public_key          = ""
   private_key         = ""
