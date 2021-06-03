357e8e7071df4987d1f0e431c0e553bcd251d4fe
Sean Sundberg <seansund@us.ibm.com>
2021-06-03 08:30:33 -0500

    Fixes typo adding ssh rule to acl (#31)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/open-acl-rules.sh
+++ scripts/open-acl-rules.sh
@@ -4,5 +4,5 @@ NETWORK_ACL="$1"
 
 ## TODO more sophisiticated logic needed to 1) test for existing rules and 2) place this rule in the right order
 
-ibmcloud is network-acl-rule-add "${NETWORK_ACL}" allow inbound tpc "0.0.0.0/0" "0.0.0.0/0" --name allow-ssh-ingress --source-port-min 22 --source-port-max 22 --destination-port-min 22 --destination-port-max 22
+ibmcloud is network-acl-rule-add "${NETWORK_ACL}" allow inbound tcp "0.0.0.0/0" "0.0.0.0/0" --name allow-ssh-ingress --source-port-min 22 --source-port-max 22 --destination-port-min 22 --destination-port-max 22
 ibmcloud is network-acl-rule-add "${NETWORK_ACL}" allow inbound udp "0.0.0.0/0" "0.0.0.0/0" --name allow-vpn-ingress --source-port-min 1194 --source-port-max 1194 --destination-port-min 1194 --destination-port-max 1194
