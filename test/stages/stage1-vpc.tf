aae642b3b3340ac371e25a6d33de99f444aa8352
Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>
2022-06-10 12:38:40 -0400

    Updated stage files
    Closes #38
    
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>

--- test/stages/stage1-vpc.tf
+++ test/stages/stage1-vpc.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-vpc.git"
 
-  resource_group_id   = module.resource_group.id
+
   resource_group_name = module.resource_group.name
   region              = var.region
   name_prefix         = var.name_prefix
-  ibmcloud_api_key    = var.ibmcloud_api_key
+
 }
