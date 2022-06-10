cfc837352148554be2f8d80135db88c7a99daee2
Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>
2022-06-10 12:25:09 -0400

    Updated workflows to match current templates
    Closes #38
    
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,2 +1,23 @@
 terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    ibm = {
+      source = "ibm-cloud/ibm"
+      version = ">= 1.22.0"
+    }
+  }
+}
+
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+
+  bin_dir = "${path.cwd}/test_bin_dir"
+  clis = ["jq", "ibmcloud-is"]
+}
+
+resource local_file bin_dir {
+  filename = "${path.cwd}/.bin_dir"
+
+  content = module.setup_clis.bin_dir
 }
